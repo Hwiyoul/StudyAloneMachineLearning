ef6cb02086344ad082729975105f37108bd3698a
Hwiyoul <blackpolarbear7@gmail.com>
2022-02-01 14:48:10 +0900

    1st Feb. train set and test set

--- Ch2_Dealing with data.py
+++ Ch2_Dealing with data.py
@@ -1,7 +1,7 @@
 # 02-1 Train set and test set
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 fish_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0,
                 31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0,
@@ -12,44 +12,45 @@ fish_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 50
                 700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0, 6.7,
                 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
 
-fish_data = [[l, w] for l, w in zip(fish_length, fish_weight)] # combine fish length and fish target list
-fish_target = [1]*35 + [0]*14 # 1 means bream and 0 means smelt
-
-kn = KNeighborsClassifier()
-
-# sampling bias test and target set
-# train_input = fish_data[:35]
-# train_target = fish_target[:35]
-# test_input = fish_data[35:]
-# test_target = fish_target[35:]
+# fish_data = [[l, w] for l, w in zip(fish_length, fish_weight)] # combine fish length and fish target list
+# fish_target = [1]*35 + [0]*14 # 1 means bream and 0 means smelt
+#
+# kn = KNeighborsClassifier()
+#
+# # sampling bias test and target set
+# # train_input = fish_data[:35]
+# # train_target = fish_target[:35]
+# # test_input = fish_data[35:]
+# # test_target = fish_target[35:]
+# #
+# # kn = kn.fit(train_input, train_target)
+# # kn.score(test_input, test_target)
+# # print(kn.score(test_input, test_target))
+#
+# input_arr = np.array(fish_data)
+# target_arr = np.array(fish_target)
+#
+# np.random.seed(42)
+# index = np.arange(49)
+# np.random.shuffle(index)
+#
+# train_input = input_arr[index[0:35]]
+# train_target = target_arr[index[0:35]]
+#
+# test_input = input_arr[index[35:]]
+# test_target = target_arr[index[35:]]
+#
+# plt.scatter(train_input[:,0], train_input[0:,1])
+# plt.scatter(test_input[:,0], test_input[0:,1])
+# plt.xlabel('length(cm)')
+# plt.ylabel('weight(g)')
+# plt.title('Fish length & weight')
+# plt.show()
 #
 # kn = kn.fit(train_input, train_target)
-# kn.score(test_input, test_target)
-# print(kn.score(test_input, test_target))
-
-input_arr = np.array(fish_data)
-target_arr = np.array(fish_target)
-
-np.random.seed(42)
-index = np.arange(49)
-np.random.shuffle(index)
-
-train_input = input_arr[index[0:35]]
-train_target = target_arr[index[0:35]]
-
-test_input = input_arr[index[35:]]
-test_target = target_arr[index[35:]]
-
-plt.scatter(train_input[:,0], train_input[0:,1])
-plt.scatter(test_input[:,0], test_input[0:,1])
-plt.xlabel('length(cm)')
-plt.ylabel('weight(g)')
-plt.title('Fish length & weight')
-plt.show()
-
-kn = kn.fit(train_input, train_target)
-ans = kn.score(test_input, test_target)
-print(kn.predict([[25,150]]))
+# ans = kn.score(test_input, test_target)
+# print(kn.predict([[25,150]]))
 
 # 02-2 Data preprocessing
-fish_data = np.column_stack((fish_length, fish_target))
+fish_data = np.column_stack((fish_length, fish_weight))
+print(fish_data)
